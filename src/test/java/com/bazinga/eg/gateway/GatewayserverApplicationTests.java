5c4ed6ffb03f8d48613b2a6c7fe61de23490b8e7
msmohamed <msmohamed@elm.sa>
2022-07-17 12:19:44 +0300

    initialize gateway server

---
+++ src/test/java/com/bazinga/eg/gateway/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazinga.eg.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
